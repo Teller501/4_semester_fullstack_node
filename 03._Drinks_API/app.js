51a00121266017696a0c0becae9a9e626591a518
Teller501 <ande777j@stud.kea.dk>
2024-02-14 12:36:12 +0100

    Corrected different indentations

--- 03._Drinks_API/app.js
+++ 03._Drinks_API/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 
 let drinks = [
-    { id: 1, name: 'Coca-Cola', type: 'Soda'},
-    { id: 2, name: 'Pepsi', type: 'Soda' },
-    { id: 3, name: 'Faxe-Kondi', type: 'Soda' },
+	{ id: 1, name: 'Coca-Cola', type: 'Soda'},
+	{ id: 2, name: 'Pepsi', type: 'Soda' },
+	{ id: 3, name: 'Faxe-Kondi', type: 'Soda' },
 	{ id: 4, name: 'Tuborg Classic', type: 'Beer', alcohol_percentage: '4.7%' },
 	{ id: 5, name: 'Carlsberg Nordic', type: 'Non Alcoholic Beer' },
 	{ id: 6, name: 'Gin & Tonic', type: 'Alcoholi Beverage' },
   ];
   
   app.get('/drinks', (req, res) => {
-    res.send(drinks);
+	res.send(drinks);
   });
 
   app.get('/drinks/:id', (req, res) => {
