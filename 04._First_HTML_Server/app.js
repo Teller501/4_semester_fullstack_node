bb7aea0bf03cd73d552301e3cb27520b9c9ff10d
Teller501 <ande777j@stud.kea.dk>
2024-02-23 12:43:37 +0100

    updated publicsquare to include known people

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -2,22 +2,27 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/homepage/homepage.html");
+  res.sendFile(__dirname + "/public/homepage/homepage.html");
 });
 
 app.get("/publicsquare", (req, res) => {
-    res.sendFile(__dirname + "/public/publicSquare/publicSquare.html")
+  res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
 });
 
+const knownNames = ["Anders", "Victor"];
+
 app.get("/greeting", (req, res) => {
-    const name = req.query.name;
+  const providedName = req.query.name;
 
-    if (name === 'anders'){
-        res.send({ data: `hello ${name}` })
-    } else {
-        res.send({ data: "hello stranger" })
-    }
+  if (knownNames.includes(providedName)) {
+    res.send({ data: `hello ${providedName}` });
+  } else {
+    res.send({ data: "hello stranger" });
+  }
+});
 
+app.get("/knownpeople", (req, res) => {
+  res.send({ data: knownNames.length });
 });
 
 const PORT = 8080;
