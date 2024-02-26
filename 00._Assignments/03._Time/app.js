9ad66bfc42020b510680b27d3ec08e90e29f2f3e
Teller501 <ande777j@stud.kea.dk>
2024-02-26 10:34:35 +0100

    made first part of assignment 03._Time

---
+++ 00._Assignments/03._Time/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/homepage/homepage.html");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
