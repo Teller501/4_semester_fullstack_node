0726b476fe61d6a7e655aa3e0fccc4c6a1b74915
Teller501 <ande777j@stud.kea.dk>
2024-03-01 18:59:35 +0100

    made home page ready

--- 00._Assignments/04._Mandatory_I/app.js
+++ 00._Assignments/04._Mandatory_I/app.js
@@ -6,5 +6,9 @@ app.use(express.static("public"));
 
 import path from "path";
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+});
+
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
