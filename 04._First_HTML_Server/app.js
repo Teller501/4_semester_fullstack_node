ab7235983f7a8092bf7c4a93e7daebb4ba077e8e
Teller501 <ande777j@stud.kea.dk>
2024-03-08 12:41:15 +0100

    finished with redirection

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -10,33 +10,45 @@ const { helicopterFactory } = require("./util/helicopterFactory.js");
 console.log(helicopterFactory());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/homepage/homepage.html");
+    res.sendFile(__dirname + "/public/homepage/homepage.html");
 });
 
 app.get("/publicsquare", (req, res) => {
-  res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
+    res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
+});
+
+app.get("/treasuretrove", (req, res) => {
+    res.send({ data: "You found it!" });
+});
+
+app.get("/secretpassphrase", (req, res) => {
+    if (req.query.passphrase !== "SesameOpenUp") {
+        res.status(400).send({ data: "Wrong passphrase" });
+    } else {
+        res.redirect("/treasuretrove");
+    }
 });
 
 const knownNames = ["Anders", "Victor"];
 
 app.get("/greeting", (req, res) => {
-  const providedName = req.query.name;
+    const providedName = req.query.name;
 
-  if (knownNames.includes(providedName)) {
-    res.send({ data: `hello ${providedName}` });
-  } else {
-    res.send({ data: "hello stranger" });
-  }
+    if (knownNames.includes(providedName)) {
+        res.send({ data: `hello ${providedName}` });
+    } else {
+        res.send({ data: "hello stranger" });
+    }
 });
 
 app.get("/knownpeople", (req, res) => {
-  res.send({ data: knownNames.length });
+    res.send({ data: knownNames.length });
 });
 
 app.get("/proxy", (req, res) => {
-  fetch("https://www.google.com")
-    .then((response) => response.text())
-    .then((result) => res.send(result));
+    fetch("https://www.google.com")
+        .then((response) => response.text())
+        .then((result) => res.send(result));
 });
 
 const PORT = 8080;
