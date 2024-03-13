5fcdb5fb9a10aadcea0ae28c2e2f7dd1fdffca11
Teller501 <ande777j@stud.kea.dk>
2024-03-13 13:30:18 +0100

    set up rest of pages with ssr

--- 00._Assignments/04._Mandatory_I/app.js
+++ 00._Assignments/04._Mandatory_I/app.js
@@ -4,7 +4,15 @@ const app = express();
 
 app.use(express.static("public"));
 
-import { homepagePage, javascriptFundamentalsPage } from "./util/readPages.js";
+import {
+    homepagePage,
+    javascriptFundamentalsPage,
+    nodeFundamentalsPage,
+    expressPage,
+    restAPIPage,
+    urlAnatomyPage,
+    miscPage,
+} from "./util/readPages.js";
 
 app.get("/", (req, res) => {
     res.send(homepagePage);
@@ -12,6 +20,26 @@ app.get("/", (req, res) => {
 
 app.get("/javascript-fundamentals", (req, res) => {
     res.send(javascriptFundamentalsPage);
+});
+
+app.get("/nodejs-fundamentals", (req, res) => {
+    res.send(nodeFundamentalsPage);
+});
+
+app.get("/express", (req, res) => {
+    res.send(expressPage);
+});
+
+app.get("/rest-api", (req, res) => {
+    res.send(restAPIPage);
+});
+
+app.get("/url-anatomy", (req, res) => {
+    res.send(urlAnatomyPage);
+});
+
+app.get("/misc", (req, res) => {
+    res.send(miscPage);
 });
 
 const PORT = 8080;
--- 00._Assignments/04._Mandatory_I/util/readPages.js
+++ 00._Assignments/04._Mandatory_I/util/readPages.js
@@ -13,3 +13,40 @@ export const javascriptFundamentalsPage = renderPage(javascriptFundamentals, {
     tabTitle: "JavaScript Fundamentals",
     CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
 });
+
+const nodeFundamentals = readPage(
+    "./public/pages/nodeFundamentals/nodeFundamentals.html"
+);
+
+export const nodeFundamentalsPage = renderPage(nodeFundamentals, {
+    tabTitle: "Node.JS Fundamentals",
+    CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
+});
+
+const express = readPage("./public/pages/express/express.html");
+
+export const expressPage = renderPage(express, {
+    tabTitle: "Express.JS",
+    CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
+});
+
+const restAPI = readPage("./public/pages/rest-api/rest-api.html");
+
+export const restAPIPage = renderPage(restAPI, {
+    tabTitle: "Rest API & Fetching",
+    CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
+});
+
+const urlAnatomy = readPage("./public/pages/url-anatomy/url-anatomy.html");
+
+export const urlAnatomyPage = renderPage(urlAnatomy, {
+    tabTitle: "URL Anatomy",
+    CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
+});
+
+const misc = readPage("./public/pages/misc/misc.html");
+
+export const miscPage = renderPage(misc, {
+    tabTitle: "Misc",
+    CSSLinks: '<link rel="stylesheet" href="/assets/css/main.css">',
+});
