fdd467e5fc5ed168b7bb1c9a3770ea8505c689d3
Teller501 <ande777j@stud.kea.dk>
2024-03-22 10:12:01 +0100

    made contact function for doginder

--- 06._DogInder/app.js
+++ 06._DogInder/app.js
@@ -4,12 +4,16 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
 import matchesRouter from "./routers/matchesRouter.js";
 app.use(matchesRouter);
 
 import pagesRouter from "./routers/pagesRouter.js";
 app.use(pagesRouter);
 
+import contactRouter from "./routers/contactRouter.js";
+app.use(contactRouter);
 
 const PORT = process.env.PORT ?? 8080;
 const server = app.listen(PORT, () =>
---
+++ 06._DogInder/routers/contactRouter.js
@@ -0,0 +1,10 @@
+// api/contact
+import { Router } from "express";
+const router = Router();
+
+router.post("/api/contact", (req, res) => {
+    console.log("Form submitted", req.body);
+    res.redirect("/");
+});
+
+export default router;
