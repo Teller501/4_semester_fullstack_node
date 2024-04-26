9299c2eeb0213ee976e5f25b284cc655376fb671
Teller501 <ande777j@stud.kea.dk>
2024-04-26 19:54:43 +0200

    Add Redis configuration to .env.sample and update redisConnection.js

--- 00._Assignments/08._Mandatory_II/server/database/redisConnection.js
+++ 00._Assignments/08._Mandatory_II/server/database/redisConnection.js
@@ -1,9 +1,10 @@
 import { createClient } from 'redis';
+import "dotenv/config";
 
 const client = createClient({
-    password: '9ovYOTYGvyeZoqvee3gomuwh3YZDkPH7',
+    password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-15046.c300.eu-central-1-1.ec2.redns.redis-cloud.com',
+        host: process.env.REDIS_HOST,
         port: 15046
     }
 });
