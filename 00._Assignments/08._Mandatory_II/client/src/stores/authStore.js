d8fa5c54219b0186918ef862486cefef137cc591
Teller501 <ande777j@stud.kea.dk>
2024-04-29 18:30:58 +0200

    created persisted store for token, user and refreshtoken

--- 00._Assignments/08._Mandatory_II/client/src/stores/authStore.js
+++ 00._Assignments/08._Mandatory_II/client/src/stores/authStore.js
@@ -1,5 +1,25 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
-export const tokenStore = writable(null);
-export const refreshTokenStore = writable(null);
-export const userStore = writable(null);
+function createPersistedStore(key, startValue) {
+    const { subscribe, set, update } = writable(startValue, () => {
+        const json = localStorage.getItem(key);
+        if (json) {
+            set(JSON.parse(json));
+        }
+
+        return () => localStorage.removeItem(key);
+    });
+
+    return {
+        subscribe,
+        set: (value) => {
+            localStorage.setItem(key, JSON.stringify(value));
+            set(value);
+        },
+        update
+    };
+}
+
+export const tokenStore = createPersistedStore('token', null);
+export const refreshTokenStore = createPersistedStore('refreshToken', null);
+export const userStore = createPersistedStore('user', null);
